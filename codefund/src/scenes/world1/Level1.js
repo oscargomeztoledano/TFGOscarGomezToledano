633297bf00d424ef6069b39241c3222c7360010f
oscargomez <oscar.gomez9@alu.uclm.es>
2025-04-17 18:32:49 +0200

    todo lo realizado el 17-04

--- codefund/src/scenes/world1/Level1.js
+++ codefund/src/scenes/world1/Level1.js
@@ -1,5 +1,5 @@
 import {Scene} from 'phaser';
-
+import { createPlatform } from './platforms';
 
 export class Level1 extends Scene
 {
@@ -11,17 +11,15 @@ export class Level1 extends Scene
     preload ()
     {
         this.load.image('background1', '/assets/world1/Backgrounds/1.png');
-        this.load.spritesheet('nature', '/assets/world1/enviroments/gamekit1.png', {frameWidth: 32, frameHeight: 32});
         this.load.image('floor1', '/assets/world1/floor/floor1.png');
         this.load.image('floor2', '/assets/world1/floor/floor2.png');
         this.load.image('floor3', '/assets/world1/floor/floor3.png');
         this.load.image('floor4', '/assets/world1/floor/floor4.png');
         this.load.image('floor5', '/assets/world1/floor/floor5.png');
         this.load.image('platformL', '/assets/world1/floor/platformL.png');
-        this.load.image('platformR', '/assets/world1/floor/platformS.png');
+        this.load.image('platformR', '/assets/world1/floor/platformR.png');
         this.load.image('cloud1', '/assets/world1/enviroments/cloud1.png');
         this.load.image('cloud2', '/assets/world1/enviroments/cloud2.png');
-        this.load.image('uitiles', '/assets/world1/UITileset.png');
 
         // Evento para depuración
         this.load.on('filecomplete', (key) => {
@@ -34,6 +32,10 @@ export class Level1 extends Scene
         this.background = this.add.tileSprite(0, 0, this.scale.width, this.scale.height, 'background1').setOrigin(0, 0);
         this.floor = this.physics.add.staticGroup();
 
+        // Crear plataformas
+        createPlatform(this)
+
+        // Crear el suelo
         // Lista de IDs de los suelos
         const floorTypes = ['floor1', 'floor2', 'floor3', 'floor4', 'floor5'];
         const tileWidth = 48; // Ancho de cada bloque de suelo
@@ -55,14 +57,6 @@ export class Level1 extends Scene
     }
     update()
     {
-        // Mover las nubes hacia la izquierda
-        this.children.list.forEach(child => {
-            if (child.texture && (child.texture.key === 'cloud1' || child.texture.key === 'cloud2')) {
-            child.x -= 0.2; // Velocidad de movimiento de las nubes
-            if (child.x + child.displayWidth < 0) {
-                child.x = this.scale.width; // Reaparecer en el lado derecho
-            }
-            }
-        });
+       
     }
 }
